dfa7ec41fc763d2b6a840140e153871c734d1005
Alexey <zabulonov444@yandex.ru>
2024-11-05 12:53:56 +0300

    edit exception

--- CalculatorOOP/src/main/java/org/example/Division.java
+++ CalculatorOOP/src/main/java/org/example/Division.java
@@ -2,9 +2,9 @@ package org.example;
 
 class Division extends Operation {
     @Override
-    public double calculate(double a, double b) {
+    public double calculate(double a, double b) throws ArithmeticException {
         if (b == 0) {
-            throw new ArithmeticException("Деление на ноль");
+            throw new ArithmeticException("Деление на ноль запрещено!");
         }
         return a / b;
     }
